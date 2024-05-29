5e0c89777b8bdbe7bbe19d185384bef8372485c8
Rich20067 <ruizricardo504@gmail.com>
2024-05-29 17:46:59 -0500

    cambio3

--- resources/views/MenuComidas.blade.php
+++ resources/views/MenuComidas.blade.php
@@ -4,16 +4,108 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
- 
-    <div class="d-flex justify-content-center align-items-center full-height">
-        <H1>INT  MENU</H1>
+<body class="bg-dark">
 
-        </div>
 
-    <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+    <header class="cabecera d-flex flex-column align-items-center" style="background-image: url('../storage/img-productos/roblenegro.jpg');">
+
+
+   <section class="navar">
+    <div class="d-flex justify-content-center align-items-center">
+        <img src="{{ asset('storage/img-productos/logofinal.png') }}" alt="">
+    </div>
+    
+    </section> 
+
+        <section>
+            <div class="contenedor1-txt1">
+                <h2 class="text-light">COMIDA RAPIDA</h2>
+                <img src="img/icono-comidas.png" alt="">
+            </div>
+        </section>
+
+        <section class="contenedor1">
+            <div id="" class="contenedor1-content">
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+        </section>
+
+
+        <section>
+
+            <div class="contenedor2-txt2">
+                <h2 class="text-light">POSTRES</h2>
+                <img src="img/icono-postres.png" alt="">
+            </div>
+
+        </section>
+
+        <section class="contenedor2">
+            <div id="" class="contenedor2-content">
+                <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+        </section>
+
+        <section>
+
+            <div class="cotenedor3-txt3">
+                <h2 class="text-light">BEBIDAS</h2>
+                <img src="img/icono-bebidas.png" alt="">
+            </div>
+
+        </section>
+
+        <section class="contenedor3">
+            <div id="bebida" class="contenedor3-content">
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+            <a href="{{ route('carta') }}"><img src="{{ asset('storage/img-productos/hamburguesa.jpg') }}" alt=""></a>
+        </section>
+
+
+
+   
 </body>
 </html>
